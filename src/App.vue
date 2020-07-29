d812b8d37947c4cd3fe9c3a3370b56652eab10bd
danielotieno <danielotieno.ke@gmail.com>
2020-07-29 12:47:33 +0300

    chore:create a component to display my name

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyName name="Daniel Otieno" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyName from './components/MyName';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    MyName,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 4% auto;
 }
 </style>
---
+++ src/components/MyName.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 class="name">{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName',
+  props: {
+    name: String,
+  },
+};
+</script>
+
+<style scoped>
+.name {
+  font-weight: bolder;
+  background-image: linear-gradient(120deg, #f093fb 0%, #f5576c 100%);
+  color: #ffffff;
+}
+</style>
